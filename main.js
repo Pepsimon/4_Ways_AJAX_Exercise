8edd12ac66be2d122d6a050147ae362106d348ae
Propete <objektivet@gmail.com>
2018-01-16 13:12:25 +0100

    Init repo, add files to repo, push to github

---
+++ main.js
@@ -0,0 +1,58 @@
+var url = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+var quote = document.getElementById("quote");
+
+// XHR
+
+var xhrBtn = document.getElementById("xhr");
+
+xhrBtn.addEventListener("click", function() {
+  var XHR = new XMLHttpRequest();
+  XHR.onreadystatechange = function() {
+    if(XHR.readyState == 4 && XHR.status == 200) {
+      var parsedData = JSON.parse(XHR.responseText);
+      quote.textContent = parsedData[0];
+    }
+  }
+  XHR.open("GET", url);
+  XHR.send();
+})
+
+
+// FETCH
+
+var fetchBtn = document.getElementById("fetch");
+
+fetchBtn.addEventListener("click", function() {
+  fetch(url)
+  .then(parseData)
+  .then(updateQuote)
+})
+
+function parseData (res) {
+    return res.json();
+}
+
+function updateQuote (text) {
+    quote.textContent = text[0];
+}
+
+// jQuery
+
+var jqBtn = document.getElementById('jquery');
+
+jqBtn.addEventListener('click', function() {
+    $.getJSON(url, function(res) {
+    quote.textContent = res[0];
+  })
+})
+
+// AXIOS
+
+var axiosBtn = document.getElementById('axios');
+
+axiosBtn.addEventListener('click', function() {
+  axios.get(url)
+  .then(function(res) {
+    quote.textContent = res.data[0];
+  })
+})
